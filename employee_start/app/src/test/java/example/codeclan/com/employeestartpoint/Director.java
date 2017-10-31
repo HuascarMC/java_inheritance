a0e7228fc0d111645bd37ea365fecf76acfd6832
Huascar <huascarmc@hotmail.com>
2017-10-31 17:27:41 +0000

    director class with budget

--- employee_start/app/src/test/java/example/codeclan/com/employeestartpoint/Director.java
+++ employee_start/app/src/test/java/example/codeclan/com/employeestartpoint/Director.java
@@ -4,5 +4,12 @@ package example.codeclan.com.employeestartpoint;
  * Created by huascar on 31/10/2017.
  */
 
-public class Director extends {
+public class Director extends Employee {
+    private double budget;
+
+    public Director(int id, String name, String socialSecurityNumber, double salary, double budget) {
+        super(id, name, socialSecurityNumber, salary);
+        this.budget = budget;
+
+    }
 }
